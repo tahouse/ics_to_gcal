f98bbad8e37ec5aa321d01b81cae9f1e9f6f4097
tahouse <26489166+tahouse@users.noreply.github.com>
2019-02-08 16:56:31 -0800

    fixed utf issue (cron doesn't know utf?)

--- cal.py
+++ cal.py
@@ -16,6 +16,8 @@ from google.auth.transport.requests import Request
 
 from icalevents.icalparser import parse_events
 
+local_path = os.path.dirname(os.path.realpath(__file__))
+
 def get_gcal_service():
     """Create a Google Calendar service object.
     copied from https://developers.google.com/calendar/quickstart/python
@@ -31,8 +33,8 @@ def get_gcal_service():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(os.path.join(local_path, 'token.pickle')):
+        with open(os.path.join(local_path, 'token.pickle'), 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -40,10 +42,10 @@ def get_gcal_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                os.path.join(local_path,'credentials.json'), SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(os.path.join(local_path, 'token.pickle'), 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
@@ -107,7 +109,9 @@ if __name__ == "__main__":
     print("Starting:", datetime.datetime.now())
 
     # load the configuration file
-    path_to_config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+    # path_to_config = os.path.join(, "config.json")
+    path_to_config = os.path.join(local_path, "config.json")
+
     print("Config location:", path_to_config)
     if os.path.exists(path_to_config):
         import json
--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+echo $USER >> $DIR/out.log
 
 source /anaconda3/etc/profile.d/conda.sh
 conda activate cal
 
-echo `which python`
-echo `python --version`
+echo `which python` >> $DIR/out.log
+echo `python --version` >> $DIR/out.log
 
 python -u $DIR/cal.py 2>&1 | tee -a $DIR/out.log
